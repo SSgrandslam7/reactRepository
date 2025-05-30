56fce3bd522d5b651ff28686af6666dd85b6a161
Stephen Schier <stephenjschier@gmail.com>
2025-05-30 13:20:12 -0500

    initial Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
+import React from 'react';
 import Navigation from './Navigation';
-import About from './About';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Resume from './Resume';
+//import About from './About';
+//import Portfolio from './Portfolio';
+//import Contact from './Contact';
+//import Resume from './Resume';
 
+function Header() {
+  return (
+    <header>
+      <h1>Stephen Schier</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
