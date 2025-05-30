b8ba967b3b2bed21ab606dc9049b9c3a95d36eb0
Stephen Schier <stephenjschier@gmail.com>
2025-05-30 13:22:43 -0500

    initial Project.jsx

---
+++ src/components/Project.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Project({ title, image, deployedLink, repoLink }) {
+  return (
+    <div className="project">
+      <h3>{title}</h3>
+      <img src={image} alt={`${title} screenshot`} />
+      <p>
+        <a href={deployedLink} target="_blank" rel="noreferrer">Live Site</a> | 
+        <a href={repoLink} target="_blank" rel="noreferrer">GitHub Repo</a>
+      </p>
+    </div>
+  );
+}
+
+export default Project;
