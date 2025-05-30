3e9263f57ba04ddb7eccfb8b05ca8cc59bb1d0ab
Stephen Schier <stephenjschier@gmail.com>
2025-05-30 13:22:05 -0500

    initial Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 function Footer() {
-    
+    return (
+    <footer>
+      <p>Connect with me:</p>
+      <a href="https://github.com/yourusername" target="_blank" rel="noreferrer">GitHub</a> | 
+      <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noreferrer">LinkedIn</a> | 
+      <a href="https://stackoverflow.com/users/yourid" target="_blank" rel="noreferrer">Stack Overflow</a>
+    </footer>
+  );
 }
+
+export default Footer;
