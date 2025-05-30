6c71709464c5280398f3e381df6761807ba85b08
Stephen Schier <stephenjschier@gmail.com>
2025-05-30 12:04:58 -0500

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <p>Aspiring Software Developer</p>
+            <img src="" alt="Photo of Stephen"/>
+            <p>This is where I write a short description about myself.....</p>
+        </div>
+    )
+};
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+import Navigation from './Navigation';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+
