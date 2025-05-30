f804f33b4335042ae5ebb73c855146f13f310d2b
Stephen Schier <stephenjschier@gmail.com>
2025-05-30 13:24:45 -0500

    initial Resume.jsx

---
+++ src/components/Resume.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+      <a href="/path/to/resume.pdf" download>Download Resume</a>
+      <h3>Proficiencies</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>HTML/CSS</li>
+        <li>MongoDB</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Resume;
